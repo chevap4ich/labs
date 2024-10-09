2a39fc22be814a508b5ea7e7f26a279fe103ea2e
chevap4ich <netparolya1298@gmail.com>
2024-10-09 13:30:08 +0300

    Update charsB.cpp

--- charsB.cpp
+++ charsB.cpp
@@ -8,23 +8,23 @@ int main(){
     int min_chars = 100;
     cout << "Enter string" << endl;
     cin.getline(str, 100);
-    char* words = strtok(str, " ");
-    while (words != 0){
+    char* word = strtok(str, " ");
+    while (word != 0){
         int count = 0;
         char* un_chars = new char[256];
         memset(un_chars, 0, 256);
-        for (int i = 0; i < strlen(words); i++){
-            if (!un_chars[(int)words[i]]){
-                un_chars[(int)words[i]] = 1;
+        for (int i = 0; i < strlen(word); i++){
+            if (!un_chars[(int)word[i]]){
+                un_chars[(int)word[i]] = 1;
                 count++;
             }
         }
         if (count < min_chars){
             min_chars = count;
-            strcpy(min_word, words);
+            strcpy(min_word, word);
         }
         delete[]un_chars;
-        words = strtok(0, " ");
+        word = strtok(0, " ");
     }
     cout << "The word with minimum of unique symbols: " << min_word;
 }

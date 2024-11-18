846f32253e5ddf0ef616f68e48b3be11dbea1f11
chevap4ich <netparolya1298@gmail.com>
2024-11-18 18:45:23 +0300

    class updeted

--- class/main.cpp
+++ class/main.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <iostream>
 #include "fraction.h"
 
@@ -8,15 +7,6 @@ int main(){
     Fraction f2(4, 76);
     std::cout << f1.addition(f2) << '\n';
     std::cout << f1.division(f2) << '\n';
-=======
-#include <iostream>
-#include "fraction.h"
-
-int main(){
-    Fraction f1(-35, 36);
-    std::cout << f1 << '\n';
-    Fraction f2(4, 76);
-    std::cout << f1.addition(f2) << '\n';
-    std::cout << f1.division(f2) << '\n';
->>>>>>> dda2ceb4080dc983b96e9b0d61f1618717e340ee
+    Fraction f3(f2);
+    
 }
